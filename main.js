e88898e052923a3183d2a862885767839bb89b1b
Cargalher <cargalher@gmail.com>
2021-05-18 14:57:59 +0200

    imgs added

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ const app = new Vue({
     el: '#root',
     data:{
         
+        images: [
+            "./img/elephants.jpg",
+            "./img/lion.jpg",
+            "./img/lamb.jpg",
+            "./img/giraff.jpg"
+        ]
     }
 })
